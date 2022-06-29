b3fb14a47262a3df2a6f08b589c514a870792538
ycoodr <ycoodr@gmail.com>
2022-06-29 11:15:13 +0300

    api complete

--- covidapp/views.py
+++ covidapp/views.py
@@ -16,13 +16,39 @@ response = requests.request("GET", url, headers=headers).json()
 # Create your views here.
 
 def helloworldview(request):
-    if request.method == 'POST':
-        selectedcountry = request.POST['selectedcountry']
-        print(selectedcountry) 
     noofresults = int(response['results'])
     mylist = []
-
     for x in range(0, noofresults):
         mylist.append(response['response'][x]['country'])
+
+         
+    
+        
+    
+    if request.method == 'POST':
+        selectedcountry = request.POST['selectedcountry']
+        
+        for x in range(0, noofresults):
+            if selectedcountry == response['response'][x]['country']:
+                new = response['response'][x]['cases']['new']
+                active = response['response'][x]['cases']['active']
+                critical = response['response'][x]['cases']['critical']
+                recovered = response['response'][x]['cases']['recovered']
+                total = response['response'][x]['cases']['total']
+                deaths = int(total) - int(active) - int(recovered)
+        
+        context = { 
+          'selectedcountry': selectedcountry,
+          'new' : new,
+          'active': active,
+          'critical': critical, 
+          'recovered': recovered,
+          'deaths' : deaths,
+          'total': total,
+          'mylist': mylist
+          }
+        return render(request, 'helloworld.html', context)
+    
+    
     context = { 'mylist' : mylist}
     return render(request, 'helloworld.html', context)
